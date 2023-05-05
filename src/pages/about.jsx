38ea64a8b53807e808f3c39816d12ef0178af814
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2023-05-05 09:33:53 -0700

    remove email href

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -112,11 +112,11 @@ export default function About() {
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
-                href="mailto:kvnchu@duck.com"
+                href="#0"
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
-                Email me
+                Email me at kvnchu (at) duck.com
               </SocialLink>
             </ul>
           </div>
